7e1279bf7d0b3ff390f03ff8612edf9473a82a26
aaron <alkrauss48@gmail.com>
2017-04-22 14:25:12 -0500

    Add documentation for log and onerror functions

--- lib/log.sh
+++ lib/log.sh
@@ -4,14 +4,23 @@ SUCCESS='\033[0;32m'  # Green
 INFO='\033[0;36m'     # Cyan
 NC='\033[0m'          # No Color
 
+# Log informational mesages
+# @param    {string} $1 - text to print out
+# @returns  {string}    - info-colored text
 log () {
   echo -e "${INFO}$1${NC}"
 }
 
+# Log successful mesages
+# @param    {string} $1 - text to print out
+# @returns  {string}    - success-colored text
 log-success () {
   echo -e "${SUCCESS}$1${NC}"
 }
 
+# Log error mesages
+# @param    {string} $1 - text to print out
+# @returns  {string}    - error-colored text
 log-error () {
   echo -e "${ERROR}$1${NC}"
 }
--- lib/onerror.sh
+++ lib/onerror.sh
@@ -1,4 +1,13 @@
-onerror(){
+source `dirname $0`/log.sh
+
+# Jackpot error handler. Prints out the stack trace and exits the program.
+# @param    {string} $? - Error code
+# @returns  {void}
+onerror() {
   log-error "There was an error in your script with code: $?"
-  while caller $((n++)); do :; done; # Stack trace
+
+  # Print out stack trace
+  while caller $((n++)); do :; done;
+
+  exit
 }
