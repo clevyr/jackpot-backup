b96211186ebe5ee643a76416e1c02dbf1a081170
aaron <alkrauss48@gmail.com>
2017-04-22 13:29:43 -0500

    Document helper functions

--- lib/function_exists.sh
+++ lib/function_exists.sh
@@ -1,3 +1,7 @@
+# Determines if a function exists.
+#
+# @param    {string} $1   - The function name
+# @returns  {string}      - The function if it exists, otherwise nothing
 function_exists() {
   declare -f -F $1
 }
